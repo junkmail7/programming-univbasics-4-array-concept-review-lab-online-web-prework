a8ee4d95f3e16d0e858d51b38046850dc19244f5
William Pannell <lpann5@gmail.com>
2019-10-03 05:39:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,11 +21,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min=array[0]
-  array.length.times { |index|
-  if array[index+1] < min
-    min=array[index+1]
-  end
-  }
-  return min
+  #min=array[0]
+  #array.length.times { |index|
+  #if array[index+1] < min
+  #  min=array[index+1]
+  #end
+  #}
+  return array.min
 end
