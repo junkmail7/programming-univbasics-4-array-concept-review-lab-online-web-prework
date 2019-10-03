f1f86e607c326b719fe6a0ed90bd85dc6fcce2f2
William Pannell <lpann5@gmail.com>
2019-10-03 05:21:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
-famous_cats.include?("Garfield")
+
 array.length.times { |index|
-  puts array[index]
+  if array.include?(value_to_find)
+    return array
 }
 end
 
