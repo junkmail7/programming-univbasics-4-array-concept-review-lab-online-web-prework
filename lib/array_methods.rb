38a7f2b37bf9b99ce7b5e70c77f1bf1b19161261
William Pannell <lpann5@gmail.com>
2019-10-03 05:23:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
 if array.include?(value_to_find)
   array.length.times { |index|
-  
-      }
+  if array[index]==value_to_find
+  }
 end
 
 def find_max_value(array)
